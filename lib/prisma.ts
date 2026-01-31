91967d00266754bbcf9797e7aac3196b4d8faf98
Teephyno Cutz <teephynocutz@gmail.com>
2026-01-31 14:06:24 -0600

    bug fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client/edge"
 import { PrismaNeon } from "@prisma/adapter-neon"
 
 const adapter = new PrismaNeon({
